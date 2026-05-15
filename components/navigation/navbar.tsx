8e9472d420d0fefb49414251f49787ad42b942b1
arabyhossainabid <arabyhossainabid@gmail.com>
2026-05-15 16:00:00 +0600

    Copy footer logo layout and image to navbar

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -40,8 +40,8 @@ const Navbar = () => {
       <div className="container mx-auto px-4 lg:px-8">
         <nav className="flex items-center justify-between">
           {/* Logo */}
-          <Link href="/" className="flex items-center gap-2 group/logo w-fit">
-            <div className="w-16 h-10 sm:w-20 sm:h-12 relative flex items-center justify-center transition-transform duration-300 group-hover/logo:scale-105">
+          <Link href="/" className="flex items-center gap-3 group/logo w-fit">
+            <div className="w-20 h-10 relative flex items-center justify-center transition-transform duration-300 group-hover/logo:scale-105">
               <Image
                 src="/images/icon/tranfarent_icon.png"
                 alt="BayVoy"
